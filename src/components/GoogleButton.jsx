73be8f864e82c5ff4c5e2a498660e272c8ad15f8
Ardian <113000649+ardiansetya@users.noreply.github.com>
2025-07-09 12:59:14 +0700

    create login page (#1)

---
+++ src/components/GoogleButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FcGoogle } from 'react-icons/fc';
+
+const GoogleButton = ({title}) => {
+  return (
+    <button
+      type="button"
+      className="w-full flex items-center justify-center gap-2 border border-yellow-500 py-2 rounded hover:bg-gray-100">
+      <FcGoogle className='text-2xl' />
+      Continue with {title}
+    </button>
+  );
+}
+
+export default GoogleButton
---
+++ src/pages/login.jsx
@@ -0,0 +1,25 @@
+import LoginForm from '@/components/AuthForm';
+import Image from 'next/image';
+import React from 'react'
+
+const login = () => {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-yellow-400 via-yellow-500 to-gray-300">
+      <div className="text-4xl font-bold text-gray-800 mb-8">
+        <Image src={'/images/logo.png'} width={150} height={150} />
+      </div>
+      <div className="bg-white rounded-2xl shadow-lg p-8 w-[90%] max-w-sm">
+        <LoginForm title={'Login'} desc={'Input your username and password to continue'}/>
+
+        <p className="text-center text-sm text-gray-600 mt-6">
+          Don’t Have Account?{" "}
+          <a href="#" className="text-purple-600 font-semibold hover:underline">
+            Register
+          </a>
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default login
